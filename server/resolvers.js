6d5c65a374c5b5ed93781d179058ab0a7e7a529c
Nelson Foltz <nelsfoltz@gmail.com>
2022-04-14 10:09:13 -0400

    initial commit

---
+++ server/resolvers.js
@@ -0,0 +1,27 @@
+const db = require('./db');
+
+const Query = {
+  company: (_, { id }) => db.companies.get(id),
+  job: (_, { id }) => db.jobs.get(id),
+  jobs: () => db.jobs.list(),
+};
+
+const Mutation = {
+  createJob: (_, { input }, { user }) => {
+    if (!user) {
+      throw new Error('Unauthorized');
+    }
+    const id = db.jobs.create({ ...input, companyId: user.companyId });
+    return db.jobs.get(id);
+  },
+};
+
+const Company = {
+  jobs: company => db.jobs.list().filter(job => job.companyId === company.id),
+};
+
+const Job = {
+  company: job => db.companies.get(job.companyId),
+};
+
+module.exports = { Query, Mutation, Company, Job };
